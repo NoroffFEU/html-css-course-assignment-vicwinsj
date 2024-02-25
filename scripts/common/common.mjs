49debeaf6d0f444592bb2782b786eee22af32fcf
Victor Winsjansen <victor.winsjansen@icloud.com>
2024-02-25 18:35:05 +0100

    Another attempt at fixing product url

--- scripts/common/common.mjs
+++ scripts/common/common.mjs
@@ -1,4 +1,4 @@
 export function productUrl(product) {
-  const productUrl = `https://norofffeu.github.io/html-css-course-assignment-vicwinsj/html-css-course-assignment-vicwinsj/product.html?id=${product.id}`;
+  const productUrl = `product.html?id=${product.id}`;
   window.location.href = productUrl;
 }
